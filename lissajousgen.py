217834348dbd817347d3df7bb732d57dfb6f9718
Ivanov Ivan <no_mail@email.com>
2021-05-01 20:07:00 +0300

    Comments are added

--- lissajousgen.py
+++ lissajousgen.py
@@ -6,39 +6,44 @@ from PyQt5 import QtCore
 
 from PyQt5.QtCore import *
 
+# флаг для имтации задержки при расчете
 doDelay = False
-
+#  класс для выполнения счета в отдельном потоке, для обеспечения "незамораживания" GUI
 class LJ(QObject):
     """
     Генерирует фигуры Лиссажу с заданными параметрами
     """
+# Qt сигнал завершения счета, отправляющий  результат в составе сигнала
     signalData = pyqtSignal(np.ndarray)
 
     def __init__(self, resolution=20):
         super(LJ, self).__init__()
         self.set_resolution(resolution)
-        
+        """
+        обработка задержки инициализации может быть выполнена аналогично расчету, путем выноса "расчетных" 
+        частей и отдельные функции;
+        пока инициализация происходит в основном потоке.
+        """
         # Эта задержка эмулирует процедуру инициализации следующей версии генератора.
         # Задержка будет убрана после обновления.
         # Пока не трогать.
         # P.S. В новом генераторе задержка будет только при инициализации.
         # Фигуры будут генерироваться так же быстро, как и сейчас.
-        time.sleep(1)
+        time.sleep(3)
+
+# метод, вызываемый из основного (GUI) потока, но выполняемый в отдельном потоке
     @QtCore.pyqtSlot(int,int)
     def callCalc(self, x_freq, y_freq):
         if doDelay:
             time.sleep(5)
-        t = np.linspace(0, 2 * np.pi, self._resolution)
-        x = np.sin(x_freq * t)
-        y = np.cos(y_freq * t)
         self.signalData.emit(self.doCalc( x_freq, y_freq))
-
+# метод выполняющий рассчет
     def doCalc(self, x_freq, y_freq):
         t = np.linspace(0, 2 * np.pi, self._resolution)
         x = np.sin(x_freq * t)
         y = np.cos(y_freq * t)
         return np.vstack((x, y))
-
+# наследование абстрактного метода
     def run(self):
         pass #time.sleep(10)
 
--- main.py
+++ main.py
@@ -33,16 +33,19 @@ default_settings = {
 with open("mpl.json", mode="r", encoding="utf-8") as f:
     mpl_color_dict = json.load(f)
 
+# класс для отрисовки изображений, будет востребован количестве графиков более одного.
 class DrawFigure(FigureCanvas):
     def __init__(self, parent=None, width=4, height=4, dpi=100, settings = default_settings):
         self._fig = plt.figure(figsize=(width,  height), dpi=dpi)
         super(DrawFigure, self).__init__(self._fig)
         self.setParent(parent)
-
+# отрисовывается в "растягиваемом" = Expanding режиме. но возможно нужна
+# реализация с фиксированным размером задаваемом пользователем
         FigureCanvas.setSizePolicy(self, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         FigureCanvas.updateGeometry(self)
         self._ax = self._fig.add_subplot(111)
         self._settings = settings
+# отрисовка графика
     def draw_plot(self, data, settings):
 
         # Строим график
@@ -51,32 +54,27 @@ class DrawFigure(FigureCanvas):
                       color=self._settings["color"], linewidth=self._settings["width"])
 
         plt.axis("off")
-        # Нужно, чтобы все элементы не выходили за пределы холста
-        #plt.tight_layout()
-
-        #self.resize(300, 300)
         self.draw()
 
-
+# базовый класс изменен на QWidget для облегчения масштабирования виджета с графиком
 #class LissajousWindow(QMainWindow):
 class LissajousWindow(QWidget):
     def __init__(self,  parent = None):
         super(LissajousWindow, self).__init__(parent)
 
 
-
+# контроллер потока для выполнения расчетов
         self.thread = QThread()
         self._lj = LJ()
+# передача класса в контроллер
         self._lj.moveToThread(self.thread)
+# соединение сигнала окончания расчетов с отрисовкой графика
         self._lj.signalData.connect(self.plot_lj_fig)
+# запуск метода run контроллера потока
         self.thread.start()
 
-        self.stopSpinner = QtCore.pyqtSignal(np.ndarray)
-
+# инифилизация GUI
         self.initUI()
-
-    def signalExample(self, data):
-        print(data)
 
     def initUI(self):
 
@@ -88,115 +86,148 @@ class LissajousWindow(QWidget):
         ))
         scriptDir = os.path.dirname(os.path.realpath(__file__))
         self.setWindowIcon(QtGui.QIcon(scriptDir + os.path.sep + "icon.bmp"))
-
+# инициализация словаря настроек
         self._settings = {}
 
-
+# создание главного слоя элементов интерфейса, горизонтальное построение
         self._layout = QHBoxLayout(self)
         self.setLayout(self._layout)
 
-
-
+# создания экземпляра класса-рисовальщика графика DrawFigure
         self._drw_fig= DrawFigure(self, 5, 5, 150, default_settings)
         self._drw_fig.draw_plot(self._lj.doCalc(default_settings["freq_x"],
                                                          default_settings["freq_y"]), default_settings)
-
+# добавление экземпляра класса-рисовальщика графика DrawFigure
         self._layout.addWidget(self._drw_fig)
 
-
+# создание слоя элементов управления, вертикальное построение
         self._ctrl_layout = QVBoxLayout(self)
 
+# создание слоя элементов установки частоты X, горизонтальное построение
         self._x_layout = QHBoxLayout(self)
 
+# создание пояснения для установки частоты X, добавление ее в слой, установка выравнивания
         self._x_freq_lb = QLabel("Частота X:")
         self._x_layout.addWidget(self._x_freq_lb)
         self._x_layout.setAlignment(self._x_freq_lb, Qt.AlignRight)
 
+# создание элемента текстового ввода для установки частоты X, добавление его в слой, установка выравнивания
         self._x_freq_ln_edt = QLineEdit("%d"%default_settings["freq_x"])
         self._x_freq_ln_edt.setFixedSize(30, 30)
         self._x_layout.addWidget(self._x_freq_ln_edt)
         self._x_layout.setAlignment(self._x_freq_ln_edt, Qt.AlignRight)
+
+# добавление слоя установки частоты X в слой элементов управления
         self._ctrl_layout.addLayout(self._x_layout)
 
+# создание слоя элементов установки частоты Y, горизонтальное построение
         self._y_layout = QHBoxLayout(self)
 
+# создание пояснения для установки частоты Y, добавление ее в слой, установка выравнивания
         self._y_freq_lb = QLabel("Частота Y:")
         self._y_layout.addWidget(self._y_freq_lb)
         self._y_layout.setAlignment(self._y_freq_lb, Qt.AlignRight)
 
+# создание элемента текстового ввода для установки частоты Y, добавление его в слой, установка выравнивания
         self._y_freq_ln_edt = QLineEdit("%d"%default_settings["freq_y"])
         self._y_freq_ln_edt.setFixedSize(30, 30)
         self._y_layout.addWidget(self._y_freq_ln_edt)
         self._y_layout.setAlignment(self._y_freq_ln_edt, Qt.AlignRight)
 
+# добавление слоя установки частоты Y в слой элементов управления
         self._ctrl_layout.addLayout(self._y_layout)
 
+# создание слоя элементов установки цвета, горизонтальное построение
         self._clr_layout = QHBoxLayout(self)
 
+# создание пояснения для установки цвета, добавление ее в слой, установка выравнивания
         self._clr_blb = QLabel("Цвет:")
         self._clr_layout.addWidget(self._clr_blb)
         self._clr_layout.setAlignment(self._clr_blb, Qt.AlignRight)
 
+# создание экземпляра комбо-бокс для установки цвета, добавление элементов комбо-бокс,
+# добавление ее в слой, установка размеров
         self._clr_combo = QComboBox()
         self._clr_combo.addItems(["Синий","Зелёный","Красный", "Жёлтый"])
         self._clr_combo.setFixedSize(80, 30)
         self._clr_layout.addWidget(self._clr_combo)
 
+# добавление слоя установки цвета в слой элементов управления
         self._ctrl_layout.addLayout(self._clr_layout)
 
+# создание слоя элементов установки толщины линии, горизонтальное построение
         self._thnk_layout = QHBoxLayout(self)
 
+# создание пояснения для установки толщины линии, добавление ее в слой, установка выравнивания
         self._thnk_blb = QLabel("Толщина \n линии:")
         self._thnk_layout.addWidget(self._thnk_blb)
         self._thnk_layout.setAlignment(self._thnk_blb, Qt.AlignRight)
 
+# создание экземпляра комбо-бокс для установки толщины линии, добавление элементов комбо-бокс,
+# добавление ее в слой, установка размеров
         self._thnk_combo = QComboBox()
         self._thnk_combo.addItems(["1", "2", "3", "4"])
         self._thnk_combo.setFixedSize(50, 30)
         self._thnk_layout.addWidget(self._thnk_combo)
 
+# добавление слоя установки толшины линии в слой элементов управления
         self._ctrl_layout.addLayout(self._thnk_layout)
 
+# добавление кнопки "Обновить фигуру", установка ее размеров и выравнивания
         self._update_btn = QPushButton("Обновить фигуру")
         self._update_btn.setFixedSize(120, 30)
         self._ctrl_layout.addWidget(self._update_btn)
         self._ctrl_layout.setAlignment(self._update_btn, Qt.AlignRight)
+
+# добавление кнопки "Сохранить фигуру", установка ее размеров и выравнивания
         self._save_btn = QPushButton("Сохранить фигуру")
         self._save_btn.setFixedSize(120, 30)
-
         self._ctrl_layout.addWidget(self._save_btn)
         self._ctrl_layout.setAlignment(self._save_btn, Qt.AlignRight)
+
+# создание экземпляра элемента заполнения
         self._ctrl_layout.addStretch()
 
+# добавление элемента заполнения
         self._layout.addLayout(self._ctrl_layout)
 
+# создание экземпляра элемента индикиции ожидания
         self._spinner = QtWaitingSpinner(self)
 
+# соединение сигнала нажатия кнопки  с методом-обработчиком
         self._update_btn.clicked.connect(self.plot_button_click_handler)
+
+# соединение сигнала нажатия кнопки с запуском индикации ожидания
         self._update_btn.clicked.connect(self._spinner.start)
+
+# соединение сигнала окончания расчета с остановкой индикации ожидания
         self._lj.signalData.connect(self._spinner.stop)
 
+# соединение сигнала нажатия кнопки  с методом-обработчиком
         self._save_btn.clicked.connect(self.save_button_click_handler)
-        #self._save_btn.clicked.connect(self._spinner.stop)
 
+# установка начального размера окна
         self.resize(400, 300)
+
+# установка начального положения окна
         self.move(300, 150)
+
+# отрисовка окна
         self.show()
 
 
-
+# функция обработки нажатия кнопки "Обновить фигуру"
     def plot_button_click_handler(self):
         """
         Обработчик нажатия на кнопку применения настроек
         """
-        # Получаем данные из текстовых полей
-
+        # Получаем данные из текстовых полей и обновляем словарь
         self._settings["freq_x"] = float(self._x_freq_ln_edt.text())
         self._settings["freq_y"] = float(self._y_freq_ln_edt.text())
         self._settings["color"] = mpl_color_dict[self._clr_combo.currentText()]
         self._settings["width"] = int(self._thnk_combo.currentText())
 
-        # Вызываем метод потока вычислений
+# Вызываем метод потока вычислений
         QMetaObject.invokeMethod(self._lj, 'callCalc', Qt.QueuedConnection,
                                  Q_ARG(int, self._settings["freq_x"]),
                                  Q_ARG(int, self._settings["freq_y"]))
@@ -220,12 +251,9 @@ class LissajousWindow(QWidget):
         if file_path == "":
             return
 
-        #запись изображения в файл
+#запись изображения в файл
         plt.savefig(file_path)
 
-    # def resizeEvent(self, event):
-    #     self._overlay.resize(event.size())
-    #     event.accept()
 
 
 if __name__ == "__main__":
